703f09e0a4bf9d582c781fed91c3bc417b8f00e6
Alexander Peletz <APeletz512@gmail.com>
2013-10-20 18:28:47 -0400

    changing view back to copy from notes

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,8 +1,5 @@
 <?php if(isset($user_name)): ?>
     <h1>This is the profile for <?=$user_name?></h1>
-<?php elseif(isset($this->user)): ?>
-	<h1>This is the profile for <?=$this->user?> found using the global '$this->user' variable!</h1>
-
 <?php else: ?>
     <h1>No user specified</h1>
 <?php endif; ?>
