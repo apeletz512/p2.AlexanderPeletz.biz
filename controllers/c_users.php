09252050e2e7c7181709b950b2a06e4d97da6acb
Alexander Peletz <APeletz512@gmail.com>
2013-10-20 12:36:52 -0400

    using view template

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -34,23 +34,17 @@ class users_controller extends base_controller {
     }
     */
 
-public function profile($user_name == NULL) {
+ public function profile($user_name = NULL) {
 
-    /*
-    If you look at _v_template you'll see it prints a $content variable in the <body>
-    Knowing that, let's pass our v_users_profile.php view fragment to $content so 
-    it's printed in the <body>
-    */
-    $this->template->content = View::instance('v_users_profile');
+    # Create a new View instance
+    # Do *not* include .php with the view name
+    $view = View::instance('v_users_profile');
 
-    /* $title is another variable used in _v_template to set the <title> of the page
-    $this->template->title = "Profile";
-
-    # Pass information to the view fragment
-    $this->template->content->user_name = $user_name;
+    # Pass information to the view instance
+    $view->user_name = $user_name;
 
     # Render View
-    echo $this-template;
+    echo $view;
 
 }
 
