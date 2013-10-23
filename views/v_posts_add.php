3e9b9923b4e872d4b78c29b22cc8fb23732bc4b4
Alexander Peletz <APeletz512@gmail.com>
2013-10-23 18:30:22 -0400

    posts

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,6 +1,6 @@
-<form method = 'post' action = '/posts/p_add'>
+<form method = 'POST' action = '/posts/p_add'>
 
-	<input type="text"/>
+	<input type="text" name="post"/>
 	<input type="submit"/>
 
 </form>
