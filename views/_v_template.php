0cc50ba213087c411f0347cea3dad2d9ec3ed6be
Alexander Peletz <APeletz512@gmail.com>
2013-10-23 17:55:59 -0400

    moving menus

--- views/_v_template.php
+++ views/_v_template.php
@@ -35,7 +35,7 @@
 				   		<li id="signup">
 				   				<a href='/users/signup'>Sign up</a>
 				        </li>
-				        <li id="login" class="pull-right">
+				        <li id="login" class="active pull-right">
 				            	<a href='/users/login'>Log in</a>
 				        </li>
 				     <?php endif; ?>
