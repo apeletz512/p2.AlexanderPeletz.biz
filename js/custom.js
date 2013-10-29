edccc6a01e6cbe8bc04b5e16d99066d963e10e72
Alexander Peletz <APeletz512@gmail.com>
2013-10-28 21:53:52 -0400

    updating posts/index

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 <script>
 
-var determinePage
+function activateTabs() {
+	var currentPage;
+	var currentTab;
+	var allTabs;
+	var i;
 
+	allTabs = document.getElementsByTag("li");
+	for (i = allTabs.length - 1; i >= 0; i--) {
+		if ((allTabs[i].id = "loginTab") || (allTabs[i].id = "logoutTab") {
+			allTabs[i].setAttribute("class", "pull-right");
+		}
+			
+		else {
+			allTabs[i].setAttribute("class", "");
+		}
+	}	
+
+	if (document.getElementById("loginBtn")) {
+		currentTab = document.getElementById("loginTab");
+		currentTab.setAttribute("class","active pull-right");
+}
+
+}
 </script>
