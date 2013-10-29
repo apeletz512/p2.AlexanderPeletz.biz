9ad4ba19d19fa07fee99da49af536cc4b0c11d0e
Alexander Peletz <APeletz512@gmail.com>
2013-10-28 22:52:33 -0400

    adding a bit of JS

--- js/custom.js
+++ js/custom.js
@@ -8,11 +8,9 @@ function activateTab() {
 	for (i = allTabs.length - 1; i >= 0; i--) {
 		if ((allTabs[i].id == "loginTab") || (allTabs[i].id == "logoutTab")) {
 			allTabs[i].setAttribute("class", "pull-right");
-			window.alert("if - "+ allTabs[i].id);
 		}
 		else if (allTabs[i].id) {
 			allTabs[i].setAttribute("class", "");
-			window.alert("else - "+ allTabs[i].id);
 		}
 	}	
 
