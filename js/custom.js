19ba14a44b9bd259574586d1d93d534b09fb4388
Alexander Peletz <APeletz512@gmail.com>
2013-10-28 22:37:40 -0400

    adding a bit of JS

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ var allTabs = document.getElementsByTagName("li");
 for (var i = allTabs.length-1; i >=0; i--) {
 	if (allTabs[i].id) {
 		allTabs[i].onClick = activateTab(allTabs[i].id);
-		window.alert(allTabs[i].onClick);
+		window.alert(allTabs[i].getAttribute("onClick"));
 	}
 }
 
