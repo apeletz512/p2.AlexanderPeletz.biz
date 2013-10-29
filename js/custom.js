4ffd0cf3d7edda974e54cd00ca33fbfa23a9b6db
Alexander Peletz <APeletz512@gmail.com>
2013-10-28 22:00:43 -0400

    adding a bit of JS

--- js/custom.js
+++ js/custom.js
@@ -8,16 +8,20 @@ function activateTabs() {
 
 	allTabs = document.getElementsByTag("li");
 	for (i = allTabs.length - 1; i >= 0; i--) {
+		alert(i);
 		if ((allTabs[i].id = "loginTab") || (allTabs[i].id = "logoutTab")) {
 			allTabs[i].setAttribute("class", "pull-right");
+			alert("if");
 		}
 			
 		else {
 			allTabs[i].setAttribute("class", "");
+			alert("else");
 		}
 	}	
 
 	if (document.getElementById("loginBtn")) {
+		alert("found loginBtn");
 		currentTab = document.getElementById("loginTab");
 		currentTab.setAttribute("class","active pull-right");
 	}
