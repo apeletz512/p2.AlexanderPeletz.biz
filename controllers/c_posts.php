6c00bbbfe038f71531174e47ba2e491053af8ed5
Alexander Peletz <APeletz512@gmail.com>
2013-10-30 21:27:16 -0400

    adding follow ability

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -41,20 +41,24 @@ class posts_controller extends base_controller {
 
     # Set up the View
     $this->template->content = View::instance('v_posts_index');
-    $this->template->title   = "Posts";
+    $this->template->title   = "All Posts";
 
-    # Build the query
-    $q = "SELECT 
-            posts .* , 
-            users.first_name, 
+    # Query
+    $q = 'SELECT 
+            posts.content,
+            posts.created,
+            posts.user_id AS post_user_id,
+            users_users.user_id AS follower_id,
+            users.first_name,
             users.last_name
         FROM posts
+        INNER JOIN users_users 
+            ON posts.user_id = users_users.user_id_followed
         INNER JOIN users 
             ON posts.user_id = users.user_id
-       ";
+        WHERE users_users.user_id = '.$this->user->user_id;
 
-
-    # Run the query
+    # Run the query, store the results in the variable $posts
     $posts = DB::instance(DB_NAME)->select_rows($q);
 
     # Pass data to the View
@@ -63,7 +67,7 @@ class posts_controller extends base_controller {
     # Render the View
     echo $this->template;
 
-	}
+}
 
 public function users() {
 
@@ -126,4 +130,5 @@ public function unfollow($user_id_followed) {
     Router::redirect("/posts/users");
 
 	}
+
 }
