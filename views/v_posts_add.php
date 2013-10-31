62fc6b6c4d551d2cee68888267feacf1908820d1
Alexander Peletz <APeletz512@gmail.com>
2013-10-30 22:08:49 -0400

    adding follow ability

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,10 +1,10 @@
 <form method='POST' action='/posts/p_add'>
 
-    <label for='content'>New Post:</label><br>
+    <label for='content'>Contribute a new post:</label>
     <textarea name='content' id='content'></textarea>
 
     <br><br>
-    <input type='submit' value='New post'>
+    <input type='submit' value='Contribute'>
 
 </form> 
 
