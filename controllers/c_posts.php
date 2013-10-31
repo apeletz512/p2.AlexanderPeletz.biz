0bcccb602f8c0b04e5db368d0777ed018d013f5d
Alexander Peletz <APeletz512@gmail.com>
2013-10-30 21:52:44 -0400

    adding follow ability

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -56,6 +56,7 @@ class posts_controller extends base_controller {
         INNER JOIN users 
             ON posts.user_id = users.user_id
         WHERE users_users.user_id = ".$this->user->user_id."
+        OR posts.user_id = ".$this->user->user_id."
         ORDER BY posts.created DESC";
 
     # Run the query, store the results in the variable $posts
